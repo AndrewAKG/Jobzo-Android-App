a3346f18bbdd7043f18b8f068ae8a522c8a373d1
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2017-11-30 21:22:45 +0200

    fix: setting header title

--- app/src/main/java/com/example/andrew/jobzo_android_app/MainActivity.kt
+++ app/src/main/java/com/example/andrew/jobzo_android_app/MainActivity.kt
@@ -27,8 +27,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        prefs = this.getSharedPreferences("tokens", MODE_PRIVATE)
+        setTitle("Jobzo")
         setContentView(R.layout.activity_main)
+        prefs = this.getSharedPreferences("tokens", MODE_PRIVATE)
         userInput = findViewById(R.id.input) as MessageInput
         messagesList.setAdapter(adapter)
 
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         val jobsMsg= findViewById(R.id.jobs)
         val degreesMsg= findViewById(R.id.degrees)
 
-
+        // setting listeners for buttons
         userInput!!.setInputListener(MessageInput.InputListener {
             sendMessage("https://radiant-basin-93715.herokuapp.com/chat", 0)
             true
@@ -45,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         coursesMsg.setOnClickListener { sendMessage("https://radiant-basin-93715.herokuapp.com/chat", 2) }
         degreesMsg.setOnClickListener { sendMessage("https://radiant-basin-93715.herokuapp.com/chat", 3) }
 
+        // initializing the chat
         welcomeUser("https://radiant-basin-93715.herokuapp.com/welcome")
     }
 
