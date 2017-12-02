ce7b302043e8639756a6e9ef90e34a51f0c66a79
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2017-12-02 15:59:34 +0200

    feat: checking internet connection and toasts while loading response

--- app/src/main/java/com/example/andrew/jobzo_android_app/ChatActivity.kt
+++ app/src/main/java/com/example/andrew/jobzo_android_app/ChatActivity.kt
@@ -3,6 +3,8 @@ package com.example.andrew.jobzo_android_app
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.widget.Toast
 import com.example.andrew.jobzo_android_app.models.Author
 import com.example.andrew.jobzo_android_app.models.Message
 import com.google.gson.Gson
@@ -28,14 +30,23 @@ class ChatActivity : AppCompatActivity() {
     private val server = Author("2")
     private var imageLoader: ImageLoader? = null
     private var adapter: MessagesListAdapter<Message>? = null
+    lateinit var toast: Toast
+
+    private fun showToast(text: String, length: Int){
+        toast = Toast.makeText(applicationContext, text, length)
+        toast.setGravity(Gravity.BOTTOM, 0, 50)
+        toast.show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        // setting the title of the chat activity
         val actionBar = supportActionBar
-        actionBar!!.setHomeButtonEnabled(true)
-        actionBar!!.setTitle("Jobzo")
+        actionBar!!.title = "Jobzo"
         actionBar.show()
+
         // creating preferences file
         prefs = this.getSharedPreferences("tokens", MODE_PRIVATE)
 
@@ -72,11 +83,13 @@ class ChatActivity : AppCompatActivity() {
     - make a user session for the user and send a welcome message
      */
     private fun welcomeUser(url: String) {
-        // sending request
+        // building request
         val request = Request.Builder()
                 .url(url)
                 .build()
 
+        showToast("connecting to server...", 10)
+        // sending request
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 println("Failed")
@@ -94,7 +107,8 @@ class ChatActivity : AppCompatActivity() {
                 prefsEditor.commit()
 
                 runOnUiThread {
-                    adapter!!.addToStart(Message(respMessage, "1", server, null), true);
+                    toast.cancel()
+                    adapter!!.addToStart(Message(respMessage, "1", server, null), true)
                 }
             } catch (e: JSONException){
                 println("JSON ERROR")
@@ -102,32 +116,47 @@ class ChatActivity : AppCompatActivity() {
         })
     }
 
+    // shuffling method
+    fun shuffleJsonArray(array: JSONArray): JSONArray {
+        // Implementing Fisher–Yates shuffle
+        val rnd = Random()
+        for (i in array.length() - 1 downTo 0) {
+            val j = rnd.nextInt(i + 1)
+            // Simple swap
+            val `object` = array.get(j)
+            array.put(j, array.get(i))
+            array.put(i, `object`)
+        }
+        return array
+    }
+
     /*
     @param: url of chat backend
     - makes the user send his message and gets the server response
      */
     private fun sendMessage(url: String, type: Int){
-        // getting user session from local storage
+        // getting user id from shared preferences
         val gson = Gson()
         val json2 = prefs!!.getString("token", "")
         val userSession = gson.fromJson<String>(json2, String::class.java)
 
+        // switching image buttons cases with the usual chat
         when(type){
             0 -> {
                 messageText = userInput!!.inputEditText.text.toString()
-                adapter!!.addToStart(Message(messageText!!, "1", user, null), true);
+                adapter!!.addToStart(Message(messageText!!, "1", user, null), true)
             }
             1 -> {
                 messageText = "jobs"
-                adapter!!.addToStart(Message("jobs", "1", user, null), true);
+                adapter!!.addToStart(Message("jobs", "1", user, null), true)
             }
             2 -> {
                 messageText = "courses"
-                adapter!!.addToStart(Message("courses", "1", user, null), true);
+                adapter!!.addToStart(Message("courses", "1", user, null), true)
             }
             3 -> {
                 messageText = "degrees"
-                adapter!!.addToStart(Message("degrees", "1", user, null), true);
+                adapter!!.addToStart(Message("degrees", "1", user, null), true)
             }
         }
 
@@ -135,35 +164,42 @@ class ChatActivity : AppCompatActivity() {
         val body: HashMap<String, String> = hashMapOf("message" to "")
         body.put("message", messageText.toString())
 
-        // sending request
+        // building request
         val request = Request.Builder()
                 .url(url)
                 .addHeader("Authorization",userSession)
                 .post(RequestBody.create(content, JSONObject(body).toString()))
                 .build()
 
+        showToast("loading your search results...", 10)
+        // sending request
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 println("Failed")
             }
             override fun onResponse(call: Call, response: Response) = try {
+                toast.cancel()
                 val responseBody = response.body()?.string()
                 val body = JSONObject(responseBody)
                 if (body.has("message")) {
-                    println(body.getString("message"))
                     val respMessage = body.getString("message")
                     runOnUiThread {
-                        adapter!!.addToStart(Message(respMessage, "1", server, null), true);
+                        adapter!!.addToStart(Message(respMessage, "1", server, null), true)
                     }
                 } else {
-                           runOnUiThread {
-                               var result : JSONArray = body.getJSONArray("items")
+                        runOnUiThread {
+                               // getting the items response array
+                               var items : JSONArray = body.getJSONArray("items")
                                var link :String
                                var title: String
                                var pagemap: JSONObject
                                var images : JSONArray? = null
                                var url: String
-                               println(result)
+
+                               // shuffling result
+                               val result = shuffleJsonArray(items)
+
+                               // responding to user with the required search results
                                var i=0
                                while(i<(result.length())) {
                                    pagemap = result.getJSONObject(i).getJSONObject("pagemap")
@@ -174,19 +210,19 @@ class ChatActivity : AppCompatActivity() {
                                    }
                                    if (images != null) {
                                        url = images.getJSONObject(0).getString("src")
-                                       adapter!!.addToStart(Message("", "1", server, url), true);
+                                       adapter!!.addToStart(Message("Image", "1", server, url), true)
                                    }
 
                                    title = result.getJSONObject(i).getString("title")
                                    link = result.getJSONObject(i).getString("link")
                                    var message = title + '\n' + link
-                                   adapter!!.addToStart(Message(message, "1", server, null), true);
+                                   adapter!!.addToStart(Message(message, "1", server, null), true)
                                    i++
                                }
                         }
                 }
             } catch (e: JSONException){
-                println("ERROR")
+                println(e)
             }
         })
     }
--- app/src/main/java/com/example/andrew/jobzo_android_app/MainActivity.kt
+++ app/src/main/java/com/example/andrew/jobzo_android_app/MainActivity.kt
@@ -1,21 +1,56 @@
 package com.example.andrew.jobzo_android_app
 
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT = 4000
+
+    // checking internet connection
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+    // alerting user if offline
+    private fun showAlertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Please check your internet connection")
+                .setCancelable(false)
+                .setPositiveButton(
+                        "Reload",
+                        DialogInterface.OnClickListener { _ , _ ->
+                            // reloading activity
+                            finish();
+                            startActivity(intent);
+                        }
+                )
+        val alert = builder.create()
+        alert.show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Handler().postDelayed(object:Runnable{
-            public override fun run() {
-                val home = Intent (this@MainActivity, ChatActivity::class.java)
-                startActivity(home)
-                finish()
-            }
-        },SPLASH_TIME_OUT.toLong())
+        if(!isNetworkAvailable()){
+            showAlertDialog()
+        }
+        else {
+            Handler().postDelayed(object:Runnable{
+                public override fun run() {
+                    val home = Intent (this@MainActivity, ChatActivity::class.java)
+                    startActivity(home)
+                    finish()
+                }
+            },SPLASH_TIME_OUT.toLong())
+        }
         val actionBar = supportActionBar
         actionBar!!.setHomeButtonEnabled(true)
         actionBar.hide()
